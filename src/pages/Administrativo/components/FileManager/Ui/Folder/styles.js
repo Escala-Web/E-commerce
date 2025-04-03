72adec5edeb5e3fa7515ed36d754165e34a0fb0b
GuilhermeS <suporte5@escalaweb.com.br>
2025-04-03 17:47:09 -0300

    Terminar

--- src/pages/Administrativo/components/FileManager/Ui/Folder/styles.js
+++ src/pages/Administrativo/components/FileManager/Ui/Folder/styles.js
@@ -4,26 +4,38 @@ export const Container = styled.div`
 
 display: flex;
 flex-direction: column;
-
-justify-content: space-between;
 align-items: center;
 
 width: 140px;
-height: 120px;
 
 padding: 6px;
 
 background-color: aqua;
+box
+
+border-radius: 6px;
 
 gap: 1rem;
 
 
 .folder {
 
-    
+    height: 100%;
 
     svg {
-        width: 200px;
+        font-size: 80px;
+        color: #333;
     }
+}
+
+.name {
+    margin-top: -1.6rem;
+
+    p {
+        font-size: 14px;
+        font-weight: 400;
+
+        color: #333;
+    }   
 }
 `;
